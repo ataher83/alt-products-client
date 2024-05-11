15c0ef4dffe776fd07498b2e4592a041547abdc5
ataher83 <taher83@gmail.com>
2024-05-11 11:42:15 +0600

    navbar, footer ,login, logot, register, dark-light theme & homepage with extra sections done

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -39,7 +39,6 @@ const Footer = () => {
 
                 <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
 
-
                 <a><svg fill="#2563EB" height="24" width="24" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
                 viewBox="0 0 310 310" xml:space="preserve">
                 <g id="XMLID_801_">
